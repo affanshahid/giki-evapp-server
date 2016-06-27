f4077d2b20405479dd04032609f79b61d02af8e1
Affan Shahid <affan.shahid.94@gmail.com>
2016-06-28 01:04:59 +0500

    Add news model and general validation

--- src/models/Module.js
+++ src/models/Module.js
@@ -4,13 +4,26 @@ export default function (sequelize, DataTypes) {
   return sequelize.define('Module', {
     title: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        len: [5,60]
+      }
     },
     description: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        len: [5,200]
+      }
     },
-    link: DataTypes.STRING,
+    link: {
+      type: DataTypes.STRING,
+      validate: {
+        isUrl: true
+      }
+    },
     category: DataTypes.STRING,
     fileUrl: DataTypes.STRING,
     startTime: {
