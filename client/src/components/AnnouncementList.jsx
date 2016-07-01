879c12a01aec5e3afb4371a53718e1d843a34591
Affan Shahid <affan.shahid.94@gmail.com>
2016-07-02 03:44:44 +0500

    Sort Announcements by createdAt date-time

--- client/src/components/AnnouncementList.jsx
+++ client/src/components/AnnouncementList.jsx
@@ -3,6 +3,17 @@ import Announcement from './Announcement';
 
 class AnnouncementList extends React.Component {
 
+  getSortedList() {
+    return this.props.announcements.sort((a1, a2) => {
+      const date1 = new Date(a1.get('createdAt')).getTime();
+      const date2 = new Date(a2.get('createdAt')).getTime();
+
+      if (date1 < date2) return 1;
+      if (date1 > date2) return -1;
+      return 0;
+    });
+  }
+
   render () {
     return (
       <section className="announcement-list">
@@ -10,7 +21,7 @@ class AnnouncementList extends React.Component {
         <ul>
           {
             this.props.announcements.size ?
-            this.props.announcements.map(anc => (
+            this.getSortedList().map(anc => (
               <Announcement
                 key={anc.get('id')}
                 title={anc.get('title')}
--- client/test/components/AnnouncementList_spec.jsx
+++ client/test/components/AnnouncementList_spec.jsx
@@ -17,8 +17,8 @@ describe('AnnouncementList', () => {
   it('displays a list of announcements', () => {
     const ancData = fromJS([
       {title: 'foo', description: 'bar', createdAt:'02/02/1994 12:00', id:1},
-      {title: 'baz', description: 'qux', createdAt:'02/04/1994 11:00', id:2},
-      {title: 'React', description: 'JS', createdAt:'03/02/1994 12:00', id:3}
+      {title: 'baz', description: 'qux', createdAt:'02/02/1994 12:00', id:2},
+      {title: 'React', description: 'JS', createdAt:'02/02/1994 12:00', id:3}
     ]);
 
     const wrapper = mount(<AnnouncementList announcements={ancData} />);
@@ -35,6 +35,23 @@ describe('AnnouncementList', () => {
     const wrapper = mount(<AnnouncementList announcements={data} />);
     expect(wrapper.find('Announcement')).to.have.lengthOf(0);
     expect(wrapper.text()).contains('No announcements');
+  });
+
+  it('sorts announcements by `createdAt`', () => {
+    const ancData = fromJS([
+      {title: 'third', description: 'bar', createdAt:'02/02/1994 8:00', id:1},
+      {title: 'last', description: 'bar', createdAt:'02/02/1990 8:00', id:4},
+      {title: 'first', description: 'qux', createdAt:'02/04/1995 11:00', id:2},
+      {title: 'second', description: 'JS', createdAt:'03/02/1994 12:00', id:3}
+    ]);
+
+    const wrapper = mount(<AnnouncementList announcements={ancData} />);
+    const announcements = wrapper.find('Announcement');
+    expect(announcements).to.have.lengthOf(4);
+    expect(announcements.at(0).text()).to.contain('first');
+    expect(announcements.at(1).text()).to.contain('second');
+    expect(announcements.at(2).text()).to.contain('third');
+    expect(announcements.at(3).text()).to.contain('last');
   });
 
 });
