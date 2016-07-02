099297abcfb7935c15dcad4ea73d05775b03462b
Affan Shahid <affan.shahid.94@gmail.com>
2016-07-02 07:37:00 +0500

    Clean up

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,16 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { fromJS } from 'immutable';
-import AnnouncementList from './components/AnnouncementList';
 import './styles.css';
 
-const ancList = fromJS([
-  {title: 'foo', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', createdAt:'02/02/1994 12:00', id:1},
-  {title: 'baz', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', createdAt:'02/04/1994 11:00', id:2},
-  {title: 'React', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', createdAt:'03/02/1994 12:00', id:3}
-]);
-
 ReactDOM.render(
-  <AnnouncementList announcements={ancList} />,
+  <h1>Hello World</h1>,
   document.getElementById('app')
 );
